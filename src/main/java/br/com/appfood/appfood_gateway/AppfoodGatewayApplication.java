b75a1d14550d645fb1709a87613b724f85f2951b
Allef Mizael <allef.mizael@gmail.com>
2023-01-06 18:08:17 -0300

    primeiro commit; a partir do projeto criado via Spring Initializr adicionei a dependência Spring Cloud Gateway e realizei as configurações necessárias em application.properties

---
+++ src/main/java/br/com/appfood/appfood_gateway/AppfoodGatewayApplication.java
@@ -0,0 +1,13 @@
+package br.com.appfood.appfood_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppfoodGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppfoodGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/appfood/appfood_gateway/AppfoodGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.appfood.appfood_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppfoodGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
